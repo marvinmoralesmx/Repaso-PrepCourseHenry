41adcc05b1b81f42d2d9ab90f911f8bcecfb2160
marvinmoralesmx <marvinmoralesmx@gmail.com>
2024-01-27 15:31:23 -0600

    subiendo operadores logicos

---
+++ M06 Bucles/operadores.js
@@ -0,0 +1,32 @@
+
+
+// AND
+
+function mayorYMenorYPar(num){
+    if (num > 5 && num < 10 && num % 2 === 0) console.log(true);
+    else console.log(false);
+}
+
+mayorYMenorYPar(7);
+mayorYMenorYPar(8);
+
+//OR 
+
+function operadorOr(str){
+    if (str ==="Henry" || str.length < 2 ) console.log(true);
+    else console.log(false);
+}
+
+operadorOr("Henry");
+peradorOr("Javascript");
+peradorOr("H");
+
+
+// NOT
+
+function negacion(permiso){
+    if (permiso) console.log("Tiene permiso");
+}
+
+negacion(true);
+negacion(false);
