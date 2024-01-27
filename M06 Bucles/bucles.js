9171096fe855c089ed0528e74e614f4d530ec9cf
marvinmoralesmx <marvinmoralesmx@gmail.com>
2024-01-27 16:29:19 -0600

    subiendo ejemplos de bucles

---
+++ M06 Bucles/bucles.js
@@ -0,0 +1,18 @@
+
+// Bucle for
+
+
+for (let i = 0; i < 5 ; i++){
+    suma = suma + i;
+    console.log("variable de iteración: ", i);
+}
+console.log("variable suma: ", suma);
+
+
+
+// Bucle While
+
+while (suma < 3){
+    suma = suma + 1;
+        console.log(suma);
+}
