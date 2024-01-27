c7076191ccfaf7345eb5265ea9e5348bd335d592
marvinmoralesmx <marvinmoralesmx@gmail.com>
2024-01-27 11:28:55 -0600

    Agregando ejemplos de funciones y sus formas asi como la precedencia en las asingaciones

---
+++ M05 Fundamentos Javascript/funciones.js
@@ -0,0 +1,24 @@
+
+
+//Funciones : sumaTres
+
+var sumaTres = (x) => {
+    return x + 3;
+}
+
+
+// Función: concuidadoConElConsolLog
+
+function concuidadoConElConsolLog(nombre) {
+    console.log(nombre)
+    return nombre;
+}
+
+
+
+//Precedencia de valores
+
+var a = 1;
+var b = 2;
+var c = a = b;
+console.log(c);
