49e541849e41cf359d935499b789e396a9e749bd
marvinmoralesmx <marvinmoralesmx@gmail.com>
2024-01-27 08:11:18 -0600

    Subiendo declarion de tipos de variables

---
+++ M05 Fundamentos Javascript/variables.js
@@ -0,0 +1,29 @@
+
+
+// String
+
+var bootcamp = "Henry";
+
+
+// Números
+
+var numeroEntero = 1;
+var numeroNegativo = -5;
+var numeroDecimal = 1.5;
+
+
+// Booleanos
+
+var estoyEntendiendo = true;
+var estoyAburrido = false;
+
+
+// Undefined
+
+var cajon1;
+console.log(cajon1);
+
+
+// Null
+
+var sinNada = null;
