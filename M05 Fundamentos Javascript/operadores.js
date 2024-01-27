e9764722bc5899d7a53b4e9562610111f0558228
marvinmoralesmx <marvinmoralesmx@gmail.com>
2024-01-27 09:48:56 -0600

    Subiendo tarea de operadores

---
+++ M05 Fundamentos Javascript/operadores.js
@@ -0,0 +1,25 @@
+
+
+// Suma
+
+console.log(3 + 5);
+
+//Resta
+
+console.log(14-7);
+
+// Multiplicación
+
+console.log(8 * 6);
+
+// División
+
+console.log(30 / 9);
+
+//Resto
+
+console.log(95 % 4);
+
+//Prueba de precedencia
+
+console.log(3 + 5 * 2 -8);
