b384cc90458eeac34757f782a52a7fc902600431
marvinmoralesmx <marvinmoralesmx@gmail.com>
2024-01-27 19:39:31 -0600

    subiendo ejemplos de como funcionan los arreglos

---
+++ M07 Arrays/arreglos.js
@@ -0,0 +1,18 @@
+
+
+//  Lista de Compras
+
+listaDeCompras[3]= "Tomates";
+listaDeCompras[1]= "Lechuga";
+
+
+// Ver Lista de Compras
+
+var elementoDelArray = listaDeCompras[1];
+console.log(elementoDelArray);
+
+
+// Length
+
+var nombres = ["matias","maria","diego","rosa"].length;
+console.log(nombres);
